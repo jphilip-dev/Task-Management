05f3e60ecd90b04d0136984c9adfdcea7494c0f7
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-05-05 01:08:17 +0800

    Add Test properties file

--- task-service/src/test/java/com/jphilips/task/TaskServiceApplicationTests.java
+++ task-service/src/test/java/com/jphilips/task/TaskServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.jphilips.task;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TaskServiceApplicationTests {
 
 	@Test
